be7df0708a02c1c8ca872a8b76a43d434214b3de
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-24 16:10:40 +0600

    Navbar and Footer Added

---
+++ src/Components/Shared/Footer/Footer.jsx
@@ -0,0 +1,125 @@
+import { MdFacebook } from 'react-icons/md';
+import { FaXTwitter } from 'react-icons/fa6';
+import { IoLogoGithub } from 'react-icons/io';
+import { FaDribbble } from 'react-icons/fa';
+import { BsDiscord } from 'react-icons/bs';
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="#" className="flex items-center">
+              <img src="/logo.png" className="h-8 me-3" alt="FlowBite Logo" />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Fitness Tracker
+              </span>
+            </a>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Resources
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Fitness-Tracker
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Fitness
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Follow us
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline ">
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Discord
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                Legal
+              </h2>
+              <ul className="text-gray-500 dark:text-gray-400 font-medium">
+                <li className="mb-4">
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms &amp; Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{' '}
+            <a href="#" className="hover:underline">
+              Fitness-Tracker™
+            </a>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+            >
+              <MdFacebook></MdFacebook>
+              <span className="sr-only">Facebook page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <BsDiscord></BsDiscord>
+              <span className="sr-only">Discord community</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <FaXTwitter />
+              <span className="sr-only">Twitter page</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <IoLogoGithub />
+              <span className="sr-only">GitHub account</span>
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5"
+            >
+              <FaDribbble />
+              <span className="sr-only">Dribbble account</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
