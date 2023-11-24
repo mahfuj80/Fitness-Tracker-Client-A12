119369c72b06b5dc983085f3ebe152c9be7e6617
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-24 10:53:40 +0600

    auth Provider Created and alos created custom hook for axios public api access

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/prop-types */
+import { createContext, useEffect, useState } from 'react';
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
+
+import { app } from '../Firebase/firebase.config';
+import useAxiosPublic from '../hooks/useAxiosPublic';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const axiosPublic = useAxiosPublic();
+
+  //   create user with email and password
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  //   sign in a user
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // sign in with google
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // log out a user
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  //   update user profile
+  const updateUserProfile = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+
+  //   follow a user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        // get token and store client
+        const userInfo = { email: currentUser?.email };
+        axiosPublic.post('/jwt', userInfo).then((res) => {
+          if (res?.data?.token) {
+            localStorage.setItem('access-token', res.data.token);
+            setLoading(false);
+          }
+        });
+      } else {
+        // TODO: remove token (if token stored in teh client side: Local storage, caching, in memory)
+        localStorage.removeItem('access-token');
+      }
+      // console.log('Current User', currentUser);
+      // setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, [axiosPublic]);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    googleSignIn,
+    logOut,
+    updateUserProfile,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
