e3253de127b46c81f3ae867eb1708f9ccb07c803
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-24 09:17:24 +0600

    Initial Project Setup with React router, Transtack, Daisyui, tailwind, Transtack Query, and Firebae

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import './index.css';
+import { router } from './Routes/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <div className="max-w-screen-xl	mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
