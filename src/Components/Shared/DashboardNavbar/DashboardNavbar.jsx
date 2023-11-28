2e5b3161a305c9171d710da6bc92e8568d0e1c12
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-28 20:36:10 +0600

    drawer added to the Admin dashboard

--- src/Components/Shared/DashboardNavbar/DashboardNavbar.jsx
+++ src/Components/Shared/DashboardNavbar/DashboardNavbar.jsx
@@ -47,6 +47,45 @@ const DashboardNavbar = () => {
   );
   return (
     <>
+      <div className="drawer lg:drawer-open mt-[64px] top-">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          {/* Page content here */}
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-primary drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side top-[64px]">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col items-center justify-center">
+              {/* Page content here */}
+              <label
+                htmlFor="my-drawer-2"
+                className="btn btn-primary drawer-button lg:hidden"
+              >
+                Open drawer
+              </label>
+            </div>
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <div>
         <div className="navbar bg-base-200 fixed z-20 max-w-screen-xl	mx-auto top-0 rounded-lg">
           <div className="navbar-start">
