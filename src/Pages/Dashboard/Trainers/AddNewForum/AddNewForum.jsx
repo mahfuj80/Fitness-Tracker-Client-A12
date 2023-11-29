d79beed63aa17811f5fa6250a92582599658f473
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-30 00:43:15 +0600

    Forum Page Complete for Admin && Trainer

--- src/Pages/Dashboard/Trainers/AddNewForum/AddNewForum.jsx
+++ src/Pages/Dashboard/Trainers/AddNewForum/AddNewForum.jsx
@@ -1,8 +1,78 @@
+import toast from 'react-hot-toast';
+import Loader from '../../../../Components/Shared/Loading/Loader';
+import SectionTitle from '../../../../Components/Shared/SectionTitle/SectionTitle';
+import useAuth from '../../../../hooks/Auth/useAuth';
+import useAxiosSecure from '../../../../hooks/Axios/useAxiosSecure';
+import useUserRole from '../../../../hooks/User/useUserRole';
+
 const AddNewForum = () => {
+  const { user, loading } = useAuth();
+  const [role, dataLoading] = useUserRole();
+  const axiosSecure = useAxiosSecure();
+  const handleForumSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target.forumTitle.value;
+    const description = e.target.forumDescription.value;
+    const date = new Date();
+    const forumInfo = {
+      email: user?.email,
+      image: user?.photoURL,
+      title,
+      description,
+      userRole: role.role,
+      date,
+    };
+    axiosSecure.post('/forum', forumInfo).then((res) => {
+      if (res?.data?.insertedId) {
+        toast.success('Forum Added');
+        const forumForm = document.getElementById('forumForm');
+        forumForm.reset();
+      }
+    });
+  };
+
+  if (loading || dataLoading) {
+    return <Loader></Loader>;
+  }
   return (
-    <div>
-      <h1>This is AddNewForum Page</h1>
-    </div>
+    <>
+      <SectionTitle
+        shortTitle={'Forum'}
+        longTitle={'Add New Forum'}
+      ></SectionTitle>
+      <form
+        id="forumForm"
+        onSubmit={handleForumSubmit}
+        className="w-full md:px-8 px-4"
+      >
+        <label className="form-control w-full mb-8">
+          <div className="label">
+            <span className="label-text">Your Forum Title Here..</span>
+          </div>
+          <input
+            type="text"
+            name="forumTitle"
+            required
+            placeholder="Forum Title"
+            className="input input-bordered input-accent w-full"
+          />
+        </label>
+        <label className="form-control w-full mb-8">
+          <div className="label">
+            <span className="label-text">Your Forum Title Here..</span>
+          </div>
+          <textarea
+            className="textarea textarea-accent w-full"
+            name="forumDescription"
+            required
+            placeholder="Bio"
+          ></textarea>
+        </label>
+        <div className="text-center mb-8">
+          <input type="submit" value={'Submit'} className="btn btn-primary" />
+        </div>
+      </form>
+    </>
   );
 };
 
--- src/hooks/User/useUserRole.jsx
+++ src/hooks/User/useUserRole.jsx
@@ -4,7 +4,6 @@ import useAxiosSecure from '../Axios/useAxiosSecure';
 
 const useUserRole = () => {
   const { user, loading } = useAuth();
-  console.log(!loading, user);
   const axiosSecure = useAxiosSecure();
   const {
     data: role = '',
@@ -15,7 +14,6 @@ const useUserRole = () => {
     enabled: !loading,
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/role/${user?.email}`);
-      console.log(res.data);
       return res?.data;
     },
   });
